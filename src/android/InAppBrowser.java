02c7e6aa0bd6cc7b91353c9188498388b7e77fc4
Nathan Ribeiro <lynxnathan@gmail.com>
2017-12-21 03:55:32 -0200

    Call CookieSyncManager::createInstance before trying to set the cookies.

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -141,7 +141,7 @@ public class InAppBrowser extends CordovaPlugin {
 
             if (null != cookies) {
                 Iterator<?> cookiesNames = cookies.keys();
-                CookieManager.createInstance(cordova.getActivity().getApplicationContext());
+                CookieSyncManager.createInstance(cordova.getActivity().getApplicationContext());
                 while (cookiesNames.hasNext()) {
                     String key = (String)cookiesNames.next();
                     CookieManager.getInstance().setCookie(domain, key + "=" + cookies.get(key));
